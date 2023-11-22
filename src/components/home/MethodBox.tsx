f72de8cfcf892c53627f3e699e3f42ce0dc04dba
Shivam00002 <dubey02shiv@gmail.com>
2023-11-22 15:48:12 +0530

    start added home-page

---
+++ src/components/home/MethodBox.tsx
@@ -0,0 +1,16 @@
+
+import Image from "next/image";
+import React from "react";
+export const MethodBox = ({ icon, work, detail }:any) => {
+  return (
+    <div className="w-full px-2 py-4 h-fit cursor-pointer ">
+      <div className="w-[200px] border h-[200px] relative">
+      <Image src={icon} fill alt="img" />
+      </div>
+      <div className="md:w-[140px] w-[100px] mt-5 h-[1px] bg-black"></div>
+      <h2 className="md:ml-16 ml-10 md:text-[14px] text-[13px]">1.</h2>
+      <h2 className="md:text-[30px] text-[18px] ">{work}</h2>
+      <p className="md:text-[16px] text-[12px] text-[#3994ba]">{detail}</p>
+    </div>
+  );
+};
---
+++ src/components/home/data/ImgData.js
@@ -0,0 +1,63 @@
+import Image from "next/image";
+
+export const PlateformImages = ["https://www.relievefurniture.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Frelieve-image-cloud-store%2Fimage%2Fupload%2Fv1695143809%2Fitem-images%2F6ea064a7-e924-4c31-9e2a-5667f1339e3b.jpg&w=3840&q=75",
+    "https://www.relievefurniture.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Frelieve-image-cloud-store%2Fimage%2Fupload%2Fv1690275727%2Fitem-images%2F776eee8a-5936-4fa0-863e-042e34737b28.jpg&w=3840&q=75",
+    "https://www.relievefurniture.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Frelieve-image-cloud-store%2Fimage%2Fupload%2Fv1690273997%2Fitem-images%2Fa525a029-ecd9-44a2-b9c4-0e75cb37dc91.jpg&w=3840&q=75",
+    "https://www.relievefurniture.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Frelieve-image-cloud-store%2Fimage%2Fupload%2Fv1690274490%2Fitem-images%2Fe80169b2-08a1-46a2-834f-15ee3745d6f9.jpg&w=3840&q=75"]
+
+
+
+export const Solutions = [
+    {
+        img: "https://www.relievefurniture.com/_next/image?url=%2Fstatic%2Fimages%2Fv3%2Fconsultations.jpg&w=828&q=75",
+        work: "Consultations",
+        description: "Get help from a qualified member of the Relieve team to design and plan any office space with items we have in stock and arriving soon."
+    },
+
+    {
+        img: "https://www.relievefurniture.com/_next/image?url=%2Fstatic%2Fimages%2Fv3%2Fleasings.jpg&w=828&q=75",
+        work: "Leasings",
+        description: "Subscribe to as little or as much furniture as you need, with the ability to change and update any furniture when needed, for less than the price of lunch."
+    },
+    {
+        img: "https://www.relievefurniture.com/_next/image?url=%2Fstatic%2Fimages%2Fv3%2Frefurbishment.jpg&w=828&q=75",
+        work: "Refurbishment",
+        description: "Adjustment not working? Or material beginning to fray? Restore and reuse your exisiting furniture to near new quality."
+    },
+    {
+        img: "https://www.relievefurniture.com/_next/image?url=%2Fstatic%2Fimages%2Fv3%2Fupcycling.jpg&w=828&q=75",
+        work: "Upcycling",
+        description: "Or use your exisiting material to create something completely new and unique through upcycling."
+    },
+]
+
+
+
+export const BoxData = [
+    {
+        imgSrc: (
+            <Image src="/chair.svg" className="object-contain" fill alt="a chair" />
+        ),
+        work: "Business",
+        detail:
+            "Handle the sustainable removal of a few unwanted items, or even an entire office, at zero cost, all by yourself. Hassle free removals that work at your own pace.",
+    },
+    {
+        imgSrc: "https://wallpaper.dog/large/46200.jpg",
+        work: "Employee",
+        detail:
+            "Embracing flexible working? Create a private, branded portal just for your employees so they can have access to their own office furniture for home.",
+    },
+    {
+        imgSrc: "https://wallpaper.dog/large/46200.jpg",
+        work: "Employee",
+        detail:
+            "Embracing flexible working? Create a private, branded portal just for your employees so they can have access to their own office furniture for home.",
+    },
+    {
+        imgSrc: "https://wallpaper.dog/large/46200.jpg",
+        work: "Employee",
+        detail:
+            "Embracing flexible working? Create a private, branded portal just for your employees so they can have access to their own office furniture for home.",
+    },
+];
