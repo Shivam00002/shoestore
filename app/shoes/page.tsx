cbab6721c49b8207a2ded13caea3bfb50b3ed3cd
Shivam00002 <dubey02shiv@gmail.com>
2023-11-22 23:34:52 +0530

    done

--- app/shoes/page.tsx
+++ app/shoes/page.tsx
@@ -3,6 +3,9 @@ import { ShoeCard } from "@/components/ui/shoe-card";
 
 const ShoePage=async()=>{
     const data = await getAllShoes();
+
+    console.log(data)
+
     return (
         <div className="container">
             <div className="grid lg:grid-cols-3 md:mt-10 sm:grid-cols-2  gap-4">
